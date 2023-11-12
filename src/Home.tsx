0a5aa27c70747f0f84b24dbd0cf91f16b1ad5438
Francis <fmdp93@gmail.com>
2023-11-12 14:11:07 +0800

    deleted row2

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,17 @@
 import Header from './components/Nav'
-import Row2 from './components/home/Row2';
 
 const Home = () => {
+    const home_row_2_heading = "home-row-2-heading col-12 col-lg-10 py-5 pl-sm-5 mx-auto";
+    const hr2h_li = "py-3 pl-5";
+
     return (
         <div className="enst-home-container">
             <div className="green-bg-overlay">
 
             </div>
             <div className="grid row-1">
+                <Header></Header>
                 <div className="item mx-auto">
-                    <Header></Header>
                     <div className="item-wrapper">
                         <div id="site-title">
                             <div className="p-4">
@@ -20,7 +22,38 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <Row2></Row2>
+            <div className="container-fluid row-2">
+                <div className="row">
+                    <div className="col-12 col-lg-6">
+                        <div className="col-1-bg"></div>
+                        <div className="col-1-bg-overlay"></div>
+                        <div className="row">
+                            <div className={home_row_2_heading}>
+                                <h2>Do you want to</h2>
+                                <ul>
+                                    <li className={hr2h_li}>Stay young</li>
+                                    <li className={hr2h_li}>Healthy</li>
+                                    <li className={hr2h_li}>Always energetic and strong?</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-6  h-100">
+                        <div className="col-2-bg"></div>
+                        <div className="col-2-bg-overlay"></div>
+                        <div className="row h-100">
+                            <div className={home_row_2_heading}>
+                                <h2>Are you looking for a</h2>
+                                <ul>
+                                    <li className={hr2h_li}>Relief to your aches and pains?</li>
+                                    <li className={hr2h_li}>An easy cure to your ailments?</li>
+                                    <li className={hr2h_li}>Liberation from anxiety, tension and worries?</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
