9f2c3a192a8901d0df5d07999c107a4d891f3bf2
Francis <fmdp93@gmail.com>
2023-11-13 17:55:26 +0800

    Add home content

--- src/Home.tsx
+++ src/Home.tsx
@@ -54,6 +54,155 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <div className="container-fluid row-3">
+                <div className="row">
+                    <div className="col-12 py-5 p-sm-5">
+                        <h2>You've got the best choice and the right place</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid row-4">
+                <div className="col-12 py-3 py-md-5">
+                    <a href="contacts.php"><button className="call-us-now btn px-5 py-3">CALL US NOW!</button></a>
+                </div>
+            </div>
+            <div className="container-fluid row-5">
+                <div className="row">
+                    <div className="col-12 col-sm-10 col-md-5 col-xl-5 pt-5 p-md-5 mx-sm-auto">
+                        <div className="content-1">
+                            <h2 className="py-3">How would you want to be healed?</h2>
+                            <ul className="px-5 px-md-3 px-lg-5">
+                                <li>SIMPLE ?</li>
+                                <li>COMPLICATED WAY ?</li>
+                                <li> PRACTICAL ?</li>
+                                <li>IMPRACTICAL WAY ?</li>
+                                <li>DIRECT ?</li>
+                                <li>CIRCUITOUS WAY ?</li>
+                                <li>EASY ?</li>
+                                <li>HARD WAY ?</li>
+                                <li>INNOVATIVE ?</li>
+                                <li>CONVENTIONAL WAY ?</li>
+                                <li>FAST ?</li>
+                                <li>SLOW WAY ?</li>
+                                <li>ECONOMICAL ?</li>
+                                <li>EXPENSIVE WAY ?</li>
+                            </ul>
+                            <strong className="py-3 py-lg-5">You have a CHOICE and ENST assures you….</strong>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-10 col-md-7 col-xl-7 py-5 p-md-5 mx-sm-auto">
+                        <div className="content-2">
+                            <h2 className="py-3">ENST Therapy differs from other treatment / massage because:</h2>
+                            <ol>
+                                <li>It is an extraordinary powerful treatment, that patients receive relief immediately after therapy.</li>
+                                <li>There is no harm in this treatment because, no usage of drugs, chemicals of any kind.</li>
+                                <li>Believe to be extremely effective with long lasting relief.</li>
+                                <li>Anyone can learn this crafts for private or family use.</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid row-6">
+                <div className="row">
+                    <div>
+                        <div className="grid">
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h2>What Kind of AILMENTS CAN Be Helped by ENST?</h2>
+                                    <p>All ailment (diseases, pains) are the effects or the outward manifestations of a malfunction in the etheric body’s “life force” – the body electricity system. When this bio-electric system fails, ultimately the body or matter dies and it returns to dust from where it came from.</p>
+                                    <p>No chemical action of drugs can take place in the human body without bio- electricity. Among the ailments in which ENST has made great impact include:</p>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Central Nervous System</h3>
+                                    Stroke, headache (tension, migraine), vertigo, depression stress, insanity, autism, cerebral palsy, insomnia and other sleep problems
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Eyes, Ear, Nose and Throat</h3>
+                                    Vision problems: cataract, glaucoma, astigmatism, color blindness
+                                    Hearing problems: Rhinitis, deafness, tinnitus
+                                    Sinusitis
+                                    Thyroid, goiter
+                                    Muteness
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Lungs</h3>
+                                    Pulmonary diseases: asthma, snoring, hiccups, emphysema, difficulty of breathe
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Heart, Blood Vessels and Circulation</h3>
+                                    High/low blood pressure, elevated cholesterol, anemia, irregular heart beat, enlarged heart, vein/artery problems
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Stomach, Intestines, Colon</h3>
+                                    Heart burn, peptic ulcer, hyperacidity, gastritis, diarrhea, chronic indigestion or constipation, hemorrhoids
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Liver, Pancreas and Gallbladder</h3>
+                                    Hepatitis, Cirrhosis
+                                    Pancreas disorder: diabetes, blood sugar levels,
+                                    Alzheimer disease, Parkinson disease
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Reproduction and Sex, Kidney</h3>
+                                    Gender identity problems: abnormalities
+                                    Fertility and birth problems
+                                    Erectile dysfunction, dysmenorrheal, prostrate problems
+                                    Kidney and urinary problems
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-4 p-md-5">
+                                    <h3>Others</h3>
+                                    Addiction to drugs, smoking, alcohol, obesity, bulimia, cancer, tumor, cyst, leukemia, etc. Stones in the organs (kidney, gallbladder) Backache and neck pain
+                                    Frozen shoulder
+                                    Nerve impingement (pinched) – “PASMA”
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="p-5 p-sm-5">
+                                    <div className="row">
+                                        <div className="col-12 col-lg-10 col-xl-12 mx-auto">
+                                            <h3 className="pb-1 pb-sm-3">Kayo ba ay</h3>
+                                            <ul className="pl-5">
+                                                <li>B</li>
+                                                <li>Busog</li><br />
+                                                <li>G</li>
+                                                <li>Gutom</li><br />
+                                                <li>B</li>
+                                                <li>Buntis</li><br />
+                                                <li>M</li>
+                                                <li>May mens</li>
+                                            </ul>
+                                            <strong>Sorry, hindi po kayo maaaring i-therapy.</strong>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid row-4">
+                <div className="col-12 py-3 py-md-5">
+                    <a href="contacts.php"><button className="call-us-now btn px-5 py-3">CALL US NOW!</button></a>
+                </div>
+            </div>
         </div>
     );
 }
